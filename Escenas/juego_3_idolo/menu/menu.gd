0ab75dc52ebbcd8717cee1a39532507907762151
Mauro Castro <maurocastruli@gmail.com>
2026-09-09 16:00:53 -0300

    varios fixes

--- Escenas/juego_3_idolo/menu/menu.gd
+++ Escenas/juego_3_idolo/menu/menu.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_button_pressed() -> void:
 	TransitionManager.change_scene("res://Escenas/juego_3_idolo/main_scene/main.tscn")
+
+
+func _on_return_pressed() -> void:
+	TransitionManager.change_scene("res://Main_menu/MainMenu.tscn")
