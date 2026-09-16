97f68afd45ae3aec7359b126c8cb8a29f5557d4e
Mauro Castro <maurocastruli@gmail.com>
2026-09-16 14:12:17 -0300

    update grafico

--- Escenas/juego_4_mutante/items_dna/dna_loot.gd
+++ Escenas/juego_4_mutante/items_dna/dna_loot.gd
@@ -1,11 +1,13 @@
 extends Node
 #dna loot
 
+
 const DNA_1 = preload("uid://cgbfvlvfohxr7")
 const DNA_2 = preload("uid://dhsvp22tuv0hs")
 const DNA_3 = preload("uid://dyv7bjge155ni")
 const DNA_4 = preload("uid://dme67xheoeoeo")
 const DNA_5 = preload("uid://bdfta00qn381m")
+
 
 
 const DNA_POOL: Array[ItemData] = [DNA_1, DNA_2, DNA_3, DNA_4, DNA_5]
