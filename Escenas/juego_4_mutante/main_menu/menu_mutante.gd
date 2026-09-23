616148f6d2f124dcffc0d0dfbdaf68b71591e32d
Mauro Castro <maurocastruli@gmail.com>
2026-09-23 13:50:33 -0300

    last update

--- Escenas/juego_4_mutante/main_menu/menu_mutante.gd
+++ Escenas/juego_4_mutante/main_menu/menu_mutante.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var musica: AudioStreamPlayer2D = $musica
+
+func _ready() -> void:
+	musica.play()
 
 func _on_play_pressed() -> void:
 	TransitionManager.change_scene("res://Escenas/juego_4_mutante/main_4.tscn")
@@ -7,3 +11,7 @@ func _on_play_pressed() -> void:
 
 func _on_volver_pressed() -> void:
 	TransitionManager.change_to_menu()
+
+
+func _on_musica_finished() -> void:
+	musica.play()
--- Main_menu/main_menu.gd
+++ Main_menu/main_menu.gd
@@ -4,10 +4,12 @@ extends Control
 @onready var juegos: VBoxContainer = $juegos
 @onready var titulo: Label = $titulo
 @onready var start_button: Button = $Button
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 func _ready() -> void:
 	juegos.hide()
-
+	canvas_layer.show()
+	
 func _on_button_pressed() -> void:
 	start_button.hide()
 	titulo.hide()
